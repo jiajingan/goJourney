db559ff89ceb315cc762eb6d6217ba58f3e93fab
jackie gan <tuj90887@temple.edu>
2022-03-10 10:58:09 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const s string = "constant string"
+
+func main() {
+	var firstname string = "test"
+	fmt.Println(firstname)
+	var age, age1 int = 10, 2
+	fmt.Println(age + age1)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	var f string = "jacob"
+	g := "nathan"
+	fmt.Println(f + g)
+}
